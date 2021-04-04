14e686b3ccb22beb22d2b10152d4a84fdf58e5f0
leonardo <leojn8@gmail.com>
2021-04-04 02:44:46 -0300

    atual

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -7,12 +9,23 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
-  function handleSubscribe() {
-    if(!session){
-      signIn('github')
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
       return;
     }
-    
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId:sessionId});
+    } catch (err) {
+     
+      
+    }
   }
 
   return (
