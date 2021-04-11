4b5c1790de44a3241066fabefc3c9a55f8a865c6
leonardo <leojn8@gmail.com>
2021-04-11 00:24:24 -0300

    atual

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,9 +23,9 @@ export const config = {
 
 const relevantEvents = new Set([
     'checkout.session.completed',
-    'customer.subscriptions.created',
-    'customer.subscriptions.updated',
-    'customer.subscriptions.deleted',
+    'customer.subscription.created',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
 ]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -46,16 +46,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
-                    case 'customer.subscriptions.created':
-                    case 'customer.subscriptions.updated':
-                    case 'customer.subscriptions.deleted':
+                    case 'customer.subscription.created':
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
 
                         const subscription = event.data.object as Stripe.Subscription;
 
                         await saveSubscription(
                             subscription.id,
                             subscription.customer.toString(),
-                            type === 'customer.subscriptions.created',
+                            type === 'customer.subscription.created',
                         )
                         break;
                     case 'checkout.session.completed':
